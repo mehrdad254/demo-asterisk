a5f42e9cf5030b2753697a6ce762057f42b34f8f
mehrdad254 <lost.mhr69@gmail.com>
2021-11-23 15:24:08 +0330

    calling sip protocols in asterisk-java

--- src/main/java/com/mhr/asterisk/config/AsteriskConfig.java
+++ src/main/java/com/mhr/asterisk/config/AsteriskConfig.java
@@ -1,15 +1,22 @@
 package com.mhr.asterisk.config;
 
 
+import java.io.IOException;
+import java.util.UUID;
+
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
 
 import org.asteriskjava.fastagi.AgiServerThread;
 import org.asteriskjava.fastagi.DefaultAgiServer;
 import org.asteriskjava.live.DefaultAsteriskServer;
+import org.asteriskjava.manager.AuthenticationFailedException;
 import org.asteriskjava.manager.ManagerConnection;
 import org.asteriskjava.manager.ManagerConnectionFactory;
 import org.asteriskjava.manager.ManagerConnectionState;
+import org.asteriskjava.manager.TimeoutException;
+import org.asteriskjava.manager.action.OriginateAction;
+import org.asteriskjava.manager.response.ManagerResponse;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Service;
@@ -57,12 +64,30 @@ public class AsteriskConfig {
 	        agiServer = new DefaultAgiServer();
 	        agiServerThread = new AgiServerThread(agiServer);
 	        agiServerThread.startup();
-
+	        
+	        callOrginator();
+	        
 		}catch (Exception e) {
 			log.error(e.getMessage());
 		}
 	}
     
+    public ManagerResponse callOrginator() throws IOException,  AuthenticationFailedException, TimeoutException {
+    	
+    	OriginateAction originateAction = new OriginateAction();
+    	
+    	originateAction.setChannel("SIP/boby");
+    	originateAction.setContext("LocalSets");
+    	originateAction.setExten("6000");
+//    	originateAction.setCallerId("6001");
+    	originateAction.setPriority(1);
+    	originateAction.setActionId(UUID.randomUUID().toString());
+    	
+    	
+    	ManagerResponse originateResponse = managerConnection.sendAction(originateAction,30000);
+    	log.info(originateResponse.getResponse());
+    	return originateResponse;
+    }
     
     
     @PreDestroy
